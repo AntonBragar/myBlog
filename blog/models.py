2db59167b0374cdb6e0ffa6249fc79cdbe80f1fd
AntonBragar <anton.bragar@gmail.com>
2021-02-24 23:01:22 +0200

    git commit -m "adding Post model to the project"

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='post_images/')
+
+    def __str__(self):
+        return self.title
